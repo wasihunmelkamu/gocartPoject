54a2206adc577c12fe26c222c636b3337410a507
Wasihun Melkamu <melkamuwasihun45@gmail.com>
2026-01-01 02:32:18 -0900

    add the necessary change

--- app/(public)/page.jsx
+++ app/(public)/page.jsx
@@ -10,7 +10,7 @@ export default function Home() {
     return (
         <div>
             <div className="w-full flex justify-end p-4">
-                <Link href="/signup" className="bg-blue-600 text-white px-4 py-2 rounded font-semibold hover:bg-blue-700 transition">Sign Up</Link>
+                <Link href="/signup" className="bg-blue-600   text-white px-4 py-2 rounded font-semibold hover:bg-blue-700 transition">Sign Up</Link>
             </div>
             <Hero />
             <LatestProducts />
--- app/api/login/route.js
+++ app/api/login/route.js
@@ -1,4 +1,4 @@
-import { prisma } from "../../../../lib/prisma";
+import { prisma } from "../../../lib/prisma"
 import bcrypt from "bcryptjs";
 export async function POST(request) {
   const { email, password } = await request.json();
